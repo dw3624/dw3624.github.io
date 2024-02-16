d2713cff2f640bd3bfbcf242843f56cace839bc3
dongwon kang <dw3624@gmail.com>
2024-02-16 16:41:22 +0900

    feat: 타입 조정

--- blog/mdx.config.js
+++ blog/mdx.config.js
@@ -1,8 +1,35 @@
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypePrettyCode from 'rehype-pretty-code'
+import rehypeSlug from 'rehype-slug'
 import remarkGfm from 'remark-gfm'
 
 export const mdxOptions = {
 	mdxOptions: {
 		remarkPlugins: [remarkGfm],
-		rehypePlugins: [],
+		rehypePlugins: [
+			rehypeSlug,
+			[
+				rehypePrettyCode,
+				{
+					theme: 'github-dark',
+					onVisitLine(node) {
+						// Prevent lines from collapsing in `display: grid` mode, and allow empty
+						// lines to be copy/pasted
+						if (node.children.length === 0) {
+							node.children = [{ type: 'text', value: ' ' }]
+						}
+					},
+				},
+			],
+			[
+				rehypeAutolinkHeadings,
+				{
+					properties: {
+						className: ['subheading-anchor'],
+						ariaLabel: 'Link to section',
+					},
+				},
+			],
+		],
 	},
 }
