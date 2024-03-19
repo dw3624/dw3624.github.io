e9b1f9cb7dc2dacdac64817f7a208abd0fb9c216
dongwon kang <dw3624@gmail.com>
2024-03-19 16:57:05 +0900

    style: style 수정

--- _next/static/chunks/app/page-9a523ca6079a7572.js
+++ _next/static/chunks/app/page-9a523ca6079a7572.js
@@ -1,1 +1,7 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{3521:function(){}},function(n){n.O(0,[971,69,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
+;(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [931],
+  { 3521: () => {} },
+  (n) => {
+    n.O(0, [971, 69, 744], () => n((n.s = 3521))), (_N_E = n.O())
+  },
+])
