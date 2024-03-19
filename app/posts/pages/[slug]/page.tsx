b3270ac8cfc2bedeb845c55a4bdaffe1c50316de
dongwon kang <dw3624@gmail.com>
2024-03-19 16:53:15 +0900

    design: 레이아웃 수정

--- app/posts/pages/[slug]/page.tsx
+++ app/posts/pages/[slug]/page.tsx
@@ -6,47 +6,47 @@ import { MAX_POST_NUM } from '@/lib/config'
 import { getPostList, getPostNum } from '@/lib/post'
 
 export const generateStaticParams = () => {
-	const postNum = getPostNum()
-	const maxPage = Math.ceil(postNum / MAX_POST_NUM)
-	return Array.from({ length: maxPage }, (_, i) => ({ slug: `${i + 1}` }))
+  const postNum = getPostNum()
+  const maxPage = Math.ceil(postNum / MAX_POST_NUM)
+  return Array.from({ length: maxPage }, (_, i) => ({ slug: `${i + 1}` }))
 }
 
 const PostsPage = ({ params }: { params: { slug: string } }) => {
-	const posts = getPostList()
+  const posts = getPostList()
 
-	const currentPage = Number(params.slug)
-	const maxPage = Math.ceil(posts.length / MAX_POST_NUM)
-	const prevPage = currentPage - 1 > 0 ? currentPage - 1 : null
-	const nextPage = currentPage + 1 > maxPage ? null : currentPage + 1
+  const currentPage = Number(params.slug)
+  const maxPage = Math.ceil(posts.length / MAX_POST_NUM)
+  const prevPage = currentPage - 1 > 0 ? currentPage - 1 : null
+  const nextPage = currentPage + 1 > maxPage ? null : currentPage + 1
 
-	return (
-		<section className="flex flex-col justify-between h-full">
-			<div>
-				<header>
-					<h1 className="text-3xl md:text-4xl font-bold">Posts</h1>
-				</header>
-				<div className="py-6">
-					{posts
-						.slice(MAX_POST_NUM * (currentPage - 1), MAX_POST_NUM * currentPage)
-						.map((post, i) => (
-							<PostCard key={i} post={post} />
-						))}
-				</div>
-			</div>
-			<nav className="w-full flex flex-col gap-2 mt-6 mx-auto 3xl:flex-row">
-				<NavButton
-					variant="prev"
-					slug={prevPage ? `/posts/pages/${currentPage - 1}` : ''}
-					text={`Page ${currentPage - 1}`}
-				/>
-				<NavButton
-					variant="next"
-					slug={nextPage ? `/posts/pages/${currentPage + 1}` : ''}
-					text={`Page ${currentPage + 1}`}
-				/>
-			</nav>
-		</section>
-	)
+  return (
+    <section className="flex flex-col justify-between h-full">
+      <div>
+        <header>
+          <h1 className="text-4xl font-bold">Posts</h1>
+        </header>
+        <div className="py-6">
+          {posts
+            .slice(MAX_POST_NUM * (currentPage - 1), MAX_POST_NUM * currentPage)
+            .map((post, i) => (
+              <PostCard key={i} post={post} />
+            ))}
+        </div>
+      </div>
+      <nav className="w-full flex flex-col gap-2 mt-6 mx-auto 3xl:flex-row">
+        <NavButton
+          variant="prev"
+          slug={prevPage ? `/posts/pages/${currentPage - 1}` : ''}
+          text={`Page ${currentPage - 1}`}
+        />
+        <NavButton
+          variant="next"
+          slug={nextPage ? `/posts/pages/${currentPage + 1}` : ''}
+          text={`Page ${currentPage + 1}`}
+        />
+      </nav>
+    </section>
+  )
 }
 
 export default PostsPage
