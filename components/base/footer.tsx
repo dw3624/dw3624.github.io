6827ee0db8c639e73528240bc4f65e38b052a8d3
dongwon kang <dw3624@gmail.com>
2024-03-19 16:44:40 +0900

    design: header, footer 조정

--- components/base/footer.tsx
+++ components/base/footer.tsx
@@ -8,51 +8,51 @@ import { useTheme } from 'next-themes'
 import Link from 'next/link'
 
 const Footer = () => {
-	const { theme, setTheme } = useTheme()
-	const [mounted, setMounted] = React.useState(false)
-	React.useEffect(() => {
-		setMounted(true)
-	}, [])
+  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = React.useState(false)
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
 
-	return (
-		<footer className="container max-w-3xl">
-			<div className="h-20 flex justify-between items-center font-sm">
-				<Link href="https://github.com/dw3624" target="_blank">
-					<span className="inline-flex items-center justify-center text-muted-foreground hover:underline">
-						© dw3624
-					</span>
-				</Link>
-				{mounted && (
-					<div className="border">
-						<Button
-							variant={theme === 'light' ? 'secondary' : 'ghost'}
-							size="iconSm"
-							onClick={() => setTheme('light')}
-							className={theme === 'light' ? 'text-primary' : ''}
-						>
-							<Sun size={16} />
-						</Button>
-						<Button
-							variant={theme === 'system' ? 'secondary' : 'ghost'}
-							size="iconSm"
-							onClick={() => setTheme('system')}
-							className={theme === 'system' ? 'text-primary' : ''}
-						>
-							<Monitor size={16} />
-						</Button>
-						<Button
-							variant={theme === 'dark' ? 'secondary' : 'ghost'}
-							size="iconSm"
-							onClick={() => setTheme('dark')}
-							className={theme === 'dark' ? 'text-primary' : ''}
-						>
-							<Moon size={16} />
-						</Button>
-					</div>
-				)}
-			</div>
-		</footer>
-	)
+  return (
+    <footer className="container max-w-5xl">
+      <div className="flex justify-between items-end font-sm py-6">
+        <Link href="https://github.com/dw3624" target="_blank">
+          <span className="inline-flex items-center justify-center text-sm text-muted-foreground hover:underline">
+            © dw3624
+          </span>
+        </Link>
+        {mounted && (
+          <div className="border">
+            <Button
+              variant={theme === 'light' ? 'secondary' : 'ghost'}
+              size="iconSm"
+              onClick={() => setTheme('light')}
+              className={theme === 'light' ? 'text-primary' : ''}
+            >
+              <Sun size={16} />
+            </Button>
+            <Button
+              variant={theme === 'system' ? 'secondary' : 'ghost'}
+              size="iconSm"
+              onClick={() => setTheme('system')}
+              className={theme === 'system' ? 'text-primary' : ''}
+            >
+              <Monitor size={16} />
+            </Button>
+            <Button
+              variant={theme === 'dark' ? 'secondary' : 'ghost'}
+              size="iconSm"
+              onClick={() => setTheme('dark')}
+              className={theme === 'dark' ? 'text-primary' : ''}
+            >
+              <Moon size={16} />
+            </Button>
+          </div>
+        )}
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
--- components/base/header.tsx
+++ components/base/header.tsx
@@ -6,55 +6,52 @@ import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const Header = () => {
-	const pathname = usePathname()
-	const routes = [
-		{
-			href: '/posts/pages/1',
-			label: 'Posts',
-			active: pathname.startsWith('/posts'),
-		},
-		{ href: '/tags', label: 'Tags', active: pathname.startsWith('/tags') },
-		{
-			href: 'https://www.notion.so/69a26827954545ee94ba401f49b42ccb',
-			label: 'Resume',
-			active: false,
-		},
-	]
+  const pathname = usePathname()
+  const routes = [
+    // {
+    // 	href: '/posts/pages/1',
+    // 	label: 'Posts',
+    // 	active: pathname.startsWith('/posts'),
+    // },
+    { href: '/tags', label: 'Tags', active: pathname.startsWith('/tags') },
+    {
+      href: 'https://www.notion.so/69a26827954545ee94ba401f49b42ccb',
+      label: 'About',
+      active: false,
+    },
+  ]
 
-	return (
-		<header className="container max-w-3xl">
-			<div className="flex items-center justify-between h-16">
-				<div className="flex gap-8 capitalize">
-					<Link
-						href="/"
-						className="inline-flex items-center justify-center text-primary hover:text-primary/80"
-					>
-						<span className="inline-flex items-center h-full ml-2 font-bold">
-							~/
-						</span>
-					</Link>
-					<nav className="flex gap-4">
-						{routes.map((route, i) => (
-							<Link
-								key={i}
-								href={route.href}
-								target={route.href.startsWith('http') ? '_blank' : '_self'}
-								className={cn(
-									'inline-flex items-center justify-center font-semibold hover:text-foreground/80',
-									route.active ? 'text-foreground' : 'text-foreground/60',
-								)}
-							>
-								{route.label}
-							</Link>
-						))}
-					</nav>
-				</div>
-				<nav>
-					<div>Hi.</div>
-				</nav>
-			</div>
-		</header>
-	)
+  return (
+    <header className="container max-w-5xl">
+      <div className="flex items-center justify-between h-14">
+        <div className="flex gap-8 capitalize">
+          <Link
+            href="/"
+            className="inline-flex items-center justify-center text-primary hover:text-primary/80"
+          >
+            <span className="inline-flex items-center h-full ml-2 font-bold">
+              ~/
+            </span>
+          </Link>
+        </div>
+        <nav className="flex gap-4">
+          {routes.map((route, i) => (
+            <Link
+              key={i}
+              href={route.href}
+              target={route.href.startsWith('http') ? '_blank' : '_self'}
+              className={cn(
+                'text-sm inline-flex items-center justify-center font-semibold hover:text-foreground/80',
+                route.active ? 'text-primary' : 'text-foreground/60',
+              )}
+            >
+              {route.label}
+            </Link>
+          ))}
+        </nav>
+      </div>
+    </header>
+  )
 }
 
 export default Header
